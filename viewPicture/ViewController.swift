4724b137956f53d18651db895a43dbd9b969e15d
碩志 <o125463200@gmail.com>
2018-11-24 18:01:37 +0800

    Initial Commit

---
+++ viewPicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewPicture
+//
+//  Created by 王碩志 on 2018/11/24.
+//  Copyright © 2018 wangsc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
